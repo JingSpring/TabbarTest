4fded5db9a31185c5cd791728f00f2f2b98d856c
xjc <1265492985@qq.com>
2016-05-01 22:00:39 +0800

    添加了TabbarTest工程

---
+++ TabbarViewTest/TabbarViewTest/ForthViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ForthViewController.swift
+//  TabbarViewTest
+//
+//  Created by xjc on 16/5/1.
+//  Copyright © 2016年 xjc. All rights reserved.
+//
+
+import UIKit
+
+class ForthViewController: UIViewController {
+    
+    var swipeLeft:UISwipeGestureRecognizer!
+    var swipeRight:UISwipeGestureRecognizer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        swipeLeft = UISwipeGestureRecognizer(target: self, action: Selector("tappedRightButton"))
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.Left
+        self.view.addGestureRecognizer(swipeLeft)
+        swipeRight = UISwipeGestureRecognizer(target: self, action: Selector("tappedLeftButton"))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        self.view.addGestureRecognizer(swipeRight)
+        
+    }
+    
+    func tappedRightButton() {
+        
+        let selectedIndex = self.tabBarController?.selectedIndex
+        let arrayViewController = self.tabBarController?.viewControllers
+        
+        if selectedIndex! < (arrayViewController?.count)!-1 {
+            let fromView = self.tabBarController?.selectedViewController?.view
+            let toView = self.tabBarController?.storyboard?.instantiateViewControllerWithIdentifier("Forth").view
+            
+            
+            UIView.transitionFromView(fromView!, toView: toView!, duration: 0.5, options: UIViewAnimationOptions.TransitionFlipFromRight, completion: { (finished) -> Void in
+                if(finished) {
+                    self.tabBarController?.selectedIndex = 3
+                }
+            })
+            
+        }
+    }
+    
+    func tappedLeftButton() {
+        let selectedIndex = self.tabBarController?.selectedIndex
+        
+        if(selectedIndex>0) {
+            let fromView = self.tabBarController?.selectedViewController?.view
+            
+            let toView = self.tabBarController?.storyboard?.instantiateViewControllerWithIdentifier("Third").view
+            
+            UIView.transitionFromView(fromView!, toView: toView!, duration: 0.5, options: UIViewAnimationOptions.TransitionFlipFromLeft, completion: { (finished) -> Void in
+                if(finished) {
+                    self.tabBarController?.selectedIndex = 2
+                }
+            })
+            
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TabbarViewTest/TabbarViewTest/SecondViewController.swift
@@ -0,0 +1,73 @@
+//
+//  SecondViewController.swift
+//  TabbarViewTest
+//
+//  Created by xjc on 16/5/1.
+//  Copyright © 2016年 xjc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    var swipeLeft:UISwipeGestureRecognizer!
+    var swipeRight:UISwipeGestureRecognizer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        swipeLeft = UISwipeGestureRecognizer(target: self, action: Selector("tappedRightButton"))
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.Left
+        self.view.addGestureRecognizer(swipeLeft)
+        swipeRight = UISwipeGestureRecognizer(target: self, action: Selector("tappedLeftButton"))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        self.view.addGestureRecognizer(swipeRight)
+        
+    }
+    
+    
+    func tappedRightButton() {
+        
+        let selectedIndex = self.tabBarController?.selectedIndex
+        let arrayViewController = self.tabBarController?.viewControllers
+        
+        if selectedIndex! < (arrayViewController?.count)!-1 {
+            let fromView = self.tabBarController?.selectedViewController?.view
+            let toView = self.tabBarController?.storyboard?.instantiateViewControllerWithIdentifier("Third").view
+            
+            
+            UIView.transitionFromView(fromView!, toView: toView!, duration: 0.5, options: UIViewAnimationOptions.TransitionFlipFromRight, completion: { (finished) -> Void in
+                if(finished) {
+                    self.tabBarController?.selectedIndex = 2
+                }
+            })
+            
+        }
+    }
+    
+    
+    func tappedLeftButton() {
+        let selectedIndex = self.tabBarController?.selectedIndex
+        
+        if(selectedIndex>0) {
+            let fromView = self.tabBarController?.selectedViewController?.view
+            
+            let toView = self.tabBarController?.storyboard?.instantiateViewControllerWithIdentifier("First").view
+            
+            UIView.transitionFromView(fromView!, toView: toView!, duration: 0.5, options: UIViewAnimationOptions.TransitionFlipFromLeft, completion: { (finished) -> Void in
+                if(finished) {
+                    self.tabBarController?.selectedIndex = 0
+                }
+            })
+            
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
